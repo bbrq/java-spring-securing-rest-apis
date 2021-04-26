9c6bd113ef0194726b896ff1567099b1b0bedffd
Ashley <sophiawu2016@gmail.com>
2021-04-26 14:54:52 -0400

    Replace the In-memory Datasource with a JDBC Datasource

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
@@ -1,10 +1,16 @@
 package io.jzheaux.springsecurity.resolutions;
 
+import java.util.List;
+
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 @SpringBootApplication
 public class ResolutionsApplication {
@@ -13,15 +19,25 @@ public class ResolutionsApplication {
 		SpringApplication.run(ResolutionsApplication.class, args);
 	}
 
-	//override the Spring Security default UserDetailsService
+	// override the Spring Security default UserDetailsService,
 	@Bean
-	public UserDetailsService userDetailsService() {
-	    return new InMemoryUserDetailsManager(
-	        org.springframework.security.core.userdetails.User
-	            .withUsername("user")
-	            //To encode a password : leave the {bcrypt} prefix.
-	            .password("{bcrypt}$2a$10$MywQEqdZFNIYnx.Ro/VQ0ulanQAl34B5xVjK2I/SDZNVGS5tHQ08W")
-	            .authorities("resolution:read")
-	            .build());
+	// replace in-memory user store with a JDBC-based one, JdbcUserDetailsManager.
+	public UserDetailsService userDetailsService(DataSource dataSource) {
+		/*
+		 * return new InMemoryUserDetailsManager(
+		 * org.springframework.security.core.userdetails.User .withUsername("user") //To
+		 * encode a password : leave the {bcrypt} prefix. .password(
+		 * "{bcrypt}$2a$10$MywQEqdZFNIYnx.Ro/VQ0ulanQAl34B5xVjK2I/SDZNVGS5tHQ08W")
+		 * .authorities("resolution:read") .build());
+		 */
+
+		//replace the use of InMemoryUserDetailsManager with an instance of JdbcUserDetailsManager
+		return new JdbcUserDetailsManager(dataSource) {
+			@Override
+			protected List<GrantedAuthority> loadUserAuthorities(String username) {
+				return AuthorityUtils.createAuthorityList("resolution:read");
+			}
+		};
 	}
+
 }
