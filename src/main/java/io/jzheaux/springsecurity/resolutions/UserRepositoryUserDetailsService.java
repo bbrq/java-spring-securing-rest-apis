c120d377ff6d949e199dcf2886c9e52010e48c11
Ashley <sophiawu2016@gmail.com>
2021-04-26 16:34:37 -0400

    Add UserRepository to UserRepositoryUserDetailsService

--- src/main/java/io/jzheaux/springsecurity/resolutions/UserRepositoryUserDetailsService.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/UserRepositoryUserDetailsService.java
@@ -5,12 +5,20 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 /**
- * Because a UserDetailsService should throw a UsernameNotFoundException if it can't find the user, 
-   the simplest UserDetailsService is one that does only that.
+ * Because a UserDetailsService should throw a UsernameNotFoundException if it
+ * can't find the user, the simplest UserDetailsService is one that does only
+ * that.
  */
 public class UserRepositoryUserDetailsService implements UserDetailsService {
-    @Override
-    public UserDetails loadUserByUsername(String username) {
-        throw new UsernameNotFoundException("no user");
-    }
+	// UserDetailsService to depend on Spring Data UserRepository.To do this,
+	// add UserRepository as a dependency
+	private final UserRepository users;
+	public UserRepositoryUserDetailsService(UserRepository users) {
+		this.users = users;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) {
+		throw new UsernameNotFoundException("no user");
+	}
 }
