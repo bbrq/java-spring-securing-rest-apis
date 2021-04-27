e8aa436f1a7a9bb73a7280c8245f6d82b1616ff8
Ashley <sophiawu2016@gmail.com>
2021-04-27 12:55:47 -0400

    turn on Method Security

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
@@ -10,7 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled=true)
 @SpringBootApplication
 public class ResolutionsApplication extends WebSecurityConfigurerAdapter{
 
