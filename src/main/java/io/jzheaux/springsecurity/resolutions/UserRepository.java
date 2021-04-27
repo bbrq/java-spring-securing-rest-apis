7f9d940c292819194fff1c04f87f3f61012fc088
Ashley <sophiawu2016@gmail.com>
2021-04-27 10:02:55 -0400

    forgot @Repository

--- src/main/java/io/jzheaux/springsecurity/resolutions/UserRepository.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/UserRepository.java
@@ -4,8 +4,10 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 
+@Repository
 public interface UserRepository extends CrudRepository<User, UUID>{
 	 Optional<User> findByUsername(String username);
 }
